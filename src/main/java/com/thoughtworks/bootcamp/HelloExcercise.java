dafc4fe9bb05ab91c76b6ffef16fbdfe956e9965
shrutipatil12 <48941864+shrutipatil12@users.noreply.github.com>
2019-09-19 11:51:27 +0530

    change print message

--- src/main/java/com/thoughtworks/bootcamp/HelloExcercise.java
+++ src/main/java/com/thoughtworks/bootcamp/HelloExcercise.java
@@ -2,7 +2,7 @@ package com.thoughtworks.bootcamp;
 
 public class HelloExcercise {
 
-  System.out.println("Hello00000000022222222222");
+  System.out.println("Hello00000000022222222222555555");
   int number;
 
 }
